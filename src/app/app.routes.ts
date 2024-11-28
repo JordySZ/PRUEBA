78dd79de7426d919a0e01d5b6290c38e37105728
JordySZ <jam.sanchez@yavirac.edu.ec>
2024-11-28 18:17:17 -0500

    Interfaz

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
-import { Deber1Component } from './deber1/deber1.component';
+
 import { Deber2Component } from './deber2/deber2.component';
 import { Deber3Component } from './deber3/deber3.component';
 import { NgModule } from '@angular/core';
 
 
 export const routes: Routes = [
-    {path:'deber1', component:Deber1Component},
+
     {path:'deber2', component:Deber2Component},
     {path:'deber3', component:Deber3Component}
 ];
