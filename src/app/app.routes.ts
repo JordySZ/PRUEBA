024bb4b1c8c607ecc47082598f534363edbb2404
JordySZ <jam.sanchez@yavirac.edu.ec>
2024-12-01 19:03:07 -0500

    DEBER_FINAL

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
 import { Deber2Component } from './DEBER 3/deber3.component';
 import { Deber3Component } from './DEBER 4/deber4.component';
 import { HomeComponent } from './home/home.component';
-import { NgModule } from '@angular/core';
 
 import { Tres } from './DEBER 2/3x3/user.component';
 import { Cuatro } from './DEBER 2/4x4/user.component';
@@ -11,20 +11,19 @@ import { Cinco } from './DEBER 2/5x5/user.component';
 import { Seis } from './DEBER 2/6x6/user.component';
 
 export const routes: Routes = [
-
-    {path:'DEBER3', component:Deber2Component},
-    {path:'DEBER4', component:Deber3Component},
-    {path:'home', component:HomeComponent}, 
-    { path: '3x3',component: Tres},
-    { path: '4x4',component: Cuatro},
-    { path: '5x5',component: Cinco},
-    { path: '6x6',component: Seis}
-  
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+  { path: 'home', component: HomeComponent }, 
+  { path: 'DEBER3', component: Deber2Component },
+  { path: 'DEBER4', component: Deber3Component },
+  { path: '3x3', component: Tres },
+  { path: '4x4', component: Cuatro },
+  { path: '5x5', component: Cinco },
+  { path: '6x6', component: Seis },
+  { path: '**', redirectTo: '/home' },
 ];
 
-
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-  export class AppRoutingModule {}
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
